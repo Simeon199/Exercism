e048d11d04eded458d7f5e5c7d25266f0640a515
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-04-26 08:59:03 +0200

    setup adjusted to use exercism from another computer

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "=== Python-Abhängigkeiten installieren ==="
+cd python && pip install -r requirements.txt && cd ..
+
+echo "=== JavaScript-Abhängigkeiten installieren ==="
+cd javascript && npm install && cd ..
+
+echo "=== Fertig! ==="
