ee6abe362fe23a6e0838f33780d7405556089e5a
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-04-29 21:30:30 +0200

    task two-fer added to sqlite track (but without solution yet)

---
+++ sqlite/two-fer/two-fer.sql
@@ -0,0 +1,9 @@
+-- Schema: CREATE TABLE "twofer" ("input" TEXT, "response" TEXT);
+-- Task: update the twofer table and set the response based on the input.
+
+CREATE TABLE twofer (
+    name TEXT,
+    uuid TEXT,
+    input TEXT,
+    response TEXT
+);
