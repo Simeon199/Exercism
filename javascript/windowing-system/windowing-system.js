30266f70f7d2e4cbc032b6118475b7a28c29701a
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-06-21 20:41:43 +0200

    feat: recent changes saved

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -5,3 +5,36 @@
  * exercise in this file. Do not forget to export the entities
  * you defined so they are available for the tests.
  */
+
+
+// @ts-ignore
+export function Size(width, height){
+    this.width = width;
+    this.height = height;
+}
+
+export function Point(){
+    this.x = 0;
+    this.y = 0;
+}
+
+export class ProgramWindow {
+    constructor(){
+        this.screenSize = {
+            width: 800,
+            height: 600
+        }
+    }
+}
+
+// @ts-ignore
+Size.prototype.resize = function(width, height){
+    this.width = width;
+    this.height = height;
+}
+
+// @ts-ignore
+Point.prototype.move = function(x, y){
+    this.x += x;
+    this.y += y;
+}
