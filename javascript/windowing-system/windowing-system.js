9a442034701faf4d50e674760109e0b62e56e769
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-06-22 21:31:33 +0200

    feat: solution to windowing-system exercise published

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -8,12 +8,12 @@
 
 
 // @ts-ignore
-export function Size(width, height){
+export function Size(width = 80, height = 60){
     this.width = width;
     this.height = height;
 }
 
-export function Point(x = 0, y = 0){
+export function Position(x = 0, y = 0){
     this.x = x;
     this.y = y;
 }
@@ -22,12 +22,28 @@ export class ProgramWindow {
     constructor(){
         this.screenSize = new Size(800, 600);
         this.size = new Size();
-        this.position = new Point();
+        this.position = new Position();
     }
 
     // @ts-ignore
     resize(newSize){
-        this.size.resize(newSize.width, newSize.height);
+        const maxWidth = this.screenSize.width - this.position.x;
+        const maxHeight = this.screenSize.height - this.position.y;
+        
+        this.size.resize(
+            Math.min(Math.max(newSize.width, 1), maxWidth),
+            Math.min(Math.max(newSize.height, 1), maxHeight)
+        );
+    }
+
+    // @ts-ignore
+    move(newPosition){
+        const maxX = this.screenSize.width - this.size.width;
+        const maxY = this.screenSize.height - this.size.height;
+        this.position.move(
+            Math.min(Math.max(newPosition.x, 0), maxX), 
+            Math.min(Math.max(newPosition.y, 0), maxY)
+        );
     }
 }
 
@@ -38,7 +54,18 @@ Size.prototype.resize = function(width, height){
 }
 
 // @ts-ignore
-Point.prototype.move = function(x, y){
-    this.x += x;
-    this.y += y;
+Position.prototype.move = function(x, y){
+    this.x = x;
+    this.y = y;
+}
+
+// @ts-ignore
+export function changeWindow(programWindow){
+    const newPosOne = new Position();
+    const newPosTwo = new Position(100, 150);
+    const newSize = new Size(400, 300);
+    programWindow.move(newPosOne);
+    programWindow.resize(newSize);
+    programWindow.move(newPosTwo);
+    return programWindow;
 }
